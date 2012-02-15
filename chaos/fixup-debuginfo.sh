99763659bc1d5d5f511257b88ce5e85bca67d89d
Mark A. Grondona <mgrondona@llnl.gov>
2012-02-15 08:56:48 -0800

    Add chaos build infrastructure

---
+++ chaos/fixup-debuginfo.sh
@@ -0,0 +1,93 @@
+#!/bin/sh
+#
+# $Id: fixup-debuginfo.sh 55 2006-08-17 15:07:48Z grondo $
+#
+# Fixup ELF debug DW_AT_comp_dir information for installed mvapich-debug RPM.
+# (Borrowed heavily from RPMs find-debuginfo.sh, but we don't want to 
+#  subsequently strip binaries)
+#
+#
+prog=$(basename $0)
+short_opts="d:p:s:b:n"
+long_opts="builddir:,destdir:,path:,srcdir:,no-check"
+
+echo "$0: called with $@"
+
+builddir=`pwd`
+srcdir=`pwd`/mvapich
+debugpath="/usr/lib/mpi/dbg/mvapich-gen2" 
+no_check=0
+
+# use the system debug for chaos5 and later
+debugedit=/usr/lib/rpm/debugedit
+
+# we use our own debug edit for chaos4
+RH_RELEASE=$(rpm -qf /etc/redhat-release --qf %{VERSION} | colrm 2)
+if [ "${RH_RELEASE}" == "5" ] ; then
+  debugedit=./debugedit/debugedit
+fi
+
+die () {
+	echo -e "$prog: $@" >&2
+	exit 1
+}
+
+set -- `/usr/bin/getopt -u -o $short_opts --long $long_opts -n $prog -- $@`
+while true; do
+    case "$1" in 
+	  -b|--builddir) builddir="$2";  shift 2 ;;
+	  -d|--destdir)  destdir="$2";   shift 2 ;;
+	  -s|--srcdir)   srcdir="$2";    shift 2 ;;
+	  -p|--path)     debugpath="$2"; shift 2 ;;
+	  -n|--no-check) no_check=1;     shift ;;
+	  --)            shift; break ;;
+	  *)             die "Unknown option" ;;
+	esac
+done
+
+SOURCEFILE=$builddir/files.debug.$$
+
+[ -z "$destdir" ] && die "--destdir not set (and not running under rpmbuild?)"
+
+echo -n > $SOURCEFILE
+
+# Move debug information for all ELF binaries from real build path to
+#  future install path. Collect all referenced sources into SOURCEFILE
+#
+for f in `find "${destdir}/${debugpath}" -type f \
+          \( -perm -0100 -or -perm -0010 -or -perm -0001 \) -exec file {} \; | \
+          sed -n -e 's/^\(.*\):[ 	]*ELF.*, not stripped/\1/p'`
+do
+    p=`echo $f | sed 's,$destdir/$debugpath,,g'`
+    echo "$p: moving srcdir from $srcdir to $debugpath/src"
+    $debugedit -b "$srcdir" -d "$debugpath/src" -l "$SOURCEFILE" "$f"
+done
+
+
+#
+# Copy referenced sources in SOURCEFILE (NUL terminated list of files) to 
+#  install location.
+#
+umask 022
+mkdir -p -m 0755 ${destdir}/${debugpath}/src
+
+cat $SOURCEFILE | \
+    ( cd $srcdir; LANG=C sort -z -u | \
+	  cpio -Lpd0m ${destdir}/${debugpath}/src ) 2>/dev/null || :
+
+rm $SOURCEFILE || die "rm $SOURCEFILE"
+
+#
+# Make sure directories and files are world readable.
+#
+chmod -R go+rX ${destdir}/${debugpath}/src  || die "chmod failed!"
+
+#
+# Make sure some sources at least were installed.
+#
+if [ $no_check = 0 ]; then
+    checksource=${destdir}/${debugpath}/src/include/mpi.h
+    [ -f $checksource ] || die "Missing source: $checksource"
+fi
+
+exit 0
